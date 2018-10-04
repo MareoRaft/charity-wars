41407e6c4aad636381ce1da7846b3684033ab41e
Matthew Lancellotti <mareoraft@gmail.com>
2018-10-03 21:31:07 -0400

    init JS contract using drizzle (web3 builtin) and then call methods for the contract. WORKING.

--- source/client/scripts/main.js
+++ source/client/scripts/main.js
@@ -1,7 +1,6 @@
 const { Drizzle, generateStore } = require('drizzle')
 
 const RoundJSON = require('../../blockchain/build/contracts/Round.json')
-// const RoundContract = new web3.eth.Contract(abi, address, {data: 'deployedBytecode' })
 
 // main JS entry point
 let p1 = '0x341FaaE3dF296544c90E12140Df6551964309395'
@@ -9,7 +8,13 @@ let p2 = '0x047EF8bcA5BB552c7907cc73A6610d32F8D93AB2'
 let p3 = '0x9c250D9764A9EcE4D64318E710c09aD79c6d0B80'
 
 // GLOBALS
-let round;
+let drizzle
+let round
+let R
+let r
+
+// HELPERS
+let log = console.log
 
 // MAIN
 function ondeploy(instance) {
@@ -41,17 +46,47 @@ function dostuff() {
 	// 	.catch(onerror)
 }
 
-function main() {
+function getData() {
+	let state = drizzle.store.getState()
+	log(state)
+	if (state.drizzleStatus.initialized) {
+		const data_key = drizzle.contracts.Round.methods.storedData.cacheCall()
+		return state.contracts.Round.methods.storedData[data_key].value
+	}
+	else {
+		return 'Loading...'
+	}
+}
+
+function init() {
 	const options = {
 	  contracts: [
 	    RoundJSON,
 	  ],
 	}
 	const drizzleStore = generateStore(options)
-	const drizzle = new Drizzle(options, drizzleStore)
+	drizzle = new Drizzle(options, drizzleStore)
+	log(drizzle.contracts)
+	log('full')
+	R = drizzle.contracts.Round
+}
 
+function init2() {
+	// more init stuff after a small delay
+	r = drizzle.contracts.Round.methods
+}
+
+function main() {
+	init()
+	setTimeout(function() {
+		init2()
+		// let data = getData()
+		// log(data)
+		r.totalPledged().call().then(function(ret) {
+			log(`total=${ret}`)
+		})
+	// 600 is enough, but not 500
+	}, 1000)
 }
 
 main()
-console.log('all done')
-
