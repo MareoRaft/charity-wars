83836638292819cf68217ce2c490c1b237f7003e
Matthew Lancellotti <mareoraft@gmail.com>
2018-10-11 13:20:08 -0400

    getting JS more organized.

--- source/client/scripts/main.js
+++ source/client/scripts/main.js
@@ -11,8 +11,11 @@ let p2 = '0x047EF8bcA5BB552c7907cc73A6610d32F8D93AB2'
 let p3 = '0x9c250D9764A9EcE4D64318E710c09aD79c6d0B80'
 
 // GLOBALS
-let round;
 const NETWORK_PORT = 7545
+let web3
+let round
+let r
+let accounts
 
 // MAIN
 function ondeploy(instance) {
@@ -31,21 +34,51 @@ function onerror(error) {
 	alert("bad bad bad")
 }
 
-async function main() {
-	let web3 = new Web3(
+function initWeb3() {
+	web3 = new Web3(
 		Web3.givenProvider ||
 		new Web3.providers.HttpProvider(`http://localhost:${NETWORK_PORT}`) ||
 		`ws://localhost:${NETWORK_PORT}`
 	)
-	let round = new web3.eth.Contract(RoundABI, RoundAddress)
-	let r = await round.methods
-	let defaultAccount = web3.eth.accounts[0]
+}
 
+function init() {
+	initWeb3()
+
+	round = new web3.eth.Contract(RoundABI, RoundAddress)
+	// Round.new() or Round.deployed() if it already exists
+	// or if it's been deployed and you know the address,
+	// Round.at("0x123...")
+	// Round.new()
+	// 	.then(ondeploy)
+	// 	.then(onsuccess)
+	// 	.catch(onerror)
+
+	r = round.methods
+
+	// none of the accounts are known to Web3 AFAIK because why would it know about all the addresses?
+	// let accounts = web3.eth.accounts
+	// let default_account = accounts[0]
+	// console.log(web3.eth.accounts)
+	// console.log(web3.eth.accounts[2])
+	// console.log(web3.eth.accounts[0])
+	// console.log(accounts[2])
+	accounts = [
+		'0x341FaaE3dF296544c90E12140Df6551964309395',
+		'0x047EF8bcA5BB552c7907cc73A6610d32F8D93AB2',
+		'0x9c250D9764A9EcE4D64318E710c09aD79c6d0B80',
+	]
+}
+
+async function main() {
+	init()
 
 	// try to pledge something!
 	// if you use await and there is an error, then the code will fail because the error is thrown instead of being fed into error1
-	let receipt = await r.pledge(4).send({from: '0x687b62fE4FAD8D6b5DF342a334d011EEC1C54bCC'}, function(error1, tx_hash){
-		console.log({error1, tx_hash})
+	let receipt = await r.pledge(1).send(
+		{from: accounts[2]},
+		function(error1, tx_hash){
+			console.log({error1, tx_hash})
 	})
 	console.log({receipt})
 
@@ -56,21 +89,6 @@ async function main() {
 		console.log({error2, result})
 	})
 	console.log({total})
-
-
-
-	// r.totalPledged().send({from: '0x2f7dcc33f538f9b2e31fb952c38b40202046aa27'})
-	// 	.on('transactionHash', function(hash){
-	// 		console.log(hash)
-	// 	})
-
-	// Round.new() or Round.deployed() if it already exists
-	// or if it's been deployed and you know the address,
-	// Round.at("0x123...")
-	// Round.new()
-	// 	.then(ondeploy)
-	// 	.then(onsuccess)
-	// 	.catch(onerror)
 }
 
 main()
